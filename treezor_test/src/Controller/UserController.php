5829ec3ef7716e5057c6585d13e2f617deb44622
notmed1 <filalimhmd@gmail.com>
2022-10-25 13:53:38 +0200

    Added new user form

--- treezor_test/src/Controller/UserController.php
+++ treezor_test/src/Controller/UserController.php
@@ -2,12 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 
 class UserController extends AbstractController
 {
@@ -19,7 +22,7 @@ class UserController extends AbstractController
      * @param Request $request
      * @return Response
      */
-    #[Route('/user', name: 'user', methods: ['GET'])]
+    #[Route('/user', name: 'user.index', methods: ['GET'])]
     public function index(UserRepository $repository, PaginatorInterface $paginator, Request $request): Response
     {
         $users = $paginator->paginate(
@@ -31,7 +34,41 @@ class UserController extends AbstractController
         return $this->render('pages/user/index.html.twig', [
             'controller_name' => 'UserController',
             'users' => $users
-
         ]);
     }
+
+    /**
+     * This controller show a form which create a new user
+     *
+     * @param Request $request
+     * @param EntityManagerInterface $manager
+     * @return Response
+     */
+    #[Route('/user/new', name: 'user', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $manager): Response
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+            $user->setActive(true);
+
+            $manager->persist($user);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                'User added succefully !'
+            );
+
+            return $this->redirectToRoute('user.index');
+        }
+
+        return $this->render('pages/user/new.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
 }
--- treezor_test/src/Form/UserType.php
+++ treezor_test/src/Form/UserType.php
@@ -4,27 +4,64 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
+use Webmozart\Assert\Assert as AssertAssert;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('last_name', TextType::class, [
-                'label' => 'label.last_name',
-                'disabled' => true,
-            ])
             ->add('first_name', TextType::class, [
-                'label' => 'label.first_name',
-                'disabled' => true,
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                'label' => 'First name',
+                'constraints' => [
+                    new Assert\NotBlank(),
+                    new Assert\Length(['min' => 2, 'max' => 50])
+                ]
+            ])
+            ->add('last_name', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                'label' => 'Last name',
+                'constraints' => [
+                    new Assert\NotBlank,
+                    new Assert\Length(['min' => 2, 'max' => 50])
+                ]
             ])
             ->add('email', EmailType::class, [
-                'label' => 'label.email',
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                'label' => 'Email',
+                'constraints' => [
+                    new Assert\NotBlank()
+                ]
             ])
-            ->add('birthday_date')
-        ;
+            ->add('birthday_date', DateType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                'label' => 'Birthday date',
+                'constraints' => [
+                    new Assert\NotBlank()
+                ]
+            ])
+            ->add('Submit', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn btn-primary mt-4'
+                ],
+                'label' => 'Add new user'
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
