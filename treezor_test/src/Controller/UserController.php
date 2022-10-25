a0f1369e875a1039f9e918e487fe4c6270fddeed
notmed1 <filalimhmd@gmail.com>
2022-10-25 13:08:24 +0200

    Added display users list

--- treezor_test/src/Controller/UserController.php
+++ treezor_test/src/Controller/UserController.php
@@ -2,17 +2,36 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\UserRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class UserController extends AbstractController
 {
-    #[Route('/user', name: 'app_user')]
-    public function index(): Response
+    /**
+     * This function display all users
+     *
+     * @param UserRepository $repository
+     * @param PaginatorInterface $paginator
+     * @param Request $request
+     * @return Response
+     */
+    #[Route('/user', name: 'user', methods: ['GET'])]
+    public function index(UserRepository $repository, PaginatorInterface $paginator, Request $request): Response
     {
+        $users = $paginator->paginate(
+            $repository->findAll(),
+            $request->query->getInt('page', 1),
+            10
+        );
+
         return $this->render('pages/user/index.html.twig', [
             'controller_name' => 'UserController',
+            'users' => $users
+
         ]);
     }
 }
